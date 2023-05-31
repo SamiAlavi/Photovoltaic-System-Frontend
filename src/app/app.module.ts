85a9fedfa0bbf246e7df37c93bff5c0ce91900dd
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-05-31 18:54:39 +0200

    add primeng module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgPrimeModule } from './ngprime.module';
 
 @NgModule({
     declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        FormsModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NgPrimeModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
