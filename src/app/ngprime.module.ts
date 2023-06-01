78716ca13439d12a9e1884a46fc753200559ad9c
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-06-01 15:47:25 +0200

    login page

--- src/app/ngprime.module.ts
+++ src/app/ngprime.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
     exports: [
+        CardModule,
+        ButtonModule,
     ],
 })
 export class NgPrimeModule { }
