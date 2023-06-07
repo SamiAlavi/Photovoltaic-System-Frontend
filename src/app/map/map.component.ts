a2c53d24cf10f0b77be598f7fbd8f46de04d8480
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-06-07 15:46:35 +0200

    move from service to component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { MapService } from '../services/map.service';
+import * as mapboxgl from 'mapbox-gl';
+import AppSettings from '../AppSettings';
+import { MAPBOX_STYLEURI } from '../helpers/enums';
 
 @Component({
     selector: 'app-map',
@@ -9,6 +12,10 @@ import { MapService } from '../services/map.service';
 export class MapComponent implements AfterViewInit, OnInit {
 
     @ViewChild('mapContainer') mapContainer!: ElementRef;
+    map!: mapboxgl.Map;
+    showControls: boolean = true;
+    private readonly STARTING_LOCATION: mapboxgl.LngLatLike = [12.9167, 50.8333]; // Chemnitz
+    private readonly STARTING_ZOOM = 10;
 
     constructor(private mapService: MapService) {
     }
@@ -17,6 +24,32 @@ export class MapComponent implements AfterViewInit, OnInit {
     }
 
     ngAfterViewInit() {
-        this.mapService.initMap(this.mapContainer);
+        this.initMap(this.mapContainer);
+    }
+
+    initMap(container: ElementRef) {
+        this.map = new mapboxgl.Map({
+            accessToken: AppSettings.MapboxAccessToken,
+            container: container.nativeElement,
+            style: MAPBOX_STYLEURI.LIGHT,
+            center: this.STARTING_LOCATION,
+            zoom: this.STARTING_ZOOM,
+            renderWorldCopies: true,
+        });
+
+        this.map.on('style.load', () => {
+            // this.mapLoad();
+        });
+
+        this.map.on('load', () => {
+            // this.mapMouse();
+            if (this.showControls) {
+                this.addControls();
+            }
+        });
+    }
+
+    private addControls() {
+        this.map.addControl(new mapboxgl.NavigationControl(), 'top-right');
     }
 }
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,42 +1,13 @@
-import { ElementRef, Injectable } from '@angular/core';
-import AppSettings from '../AppSettings';
+import { Injectable } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { MAPBOX_STYLEURI } from '../helpers/enums';
 
 @Injectable({
     providedIn: 'root'
 })
 export class MapService {
     map!: mapboxgl.Map;
-    showControls: boolean = true;
 
     constructor() {
 
-    }
-
-    initMap(container: ElementRef): void {
-        this.map = new mapboxgl.Map({
-            accessToken: AppSettings.MapboxAccessToken,
-            container: container.nativeElement,
-            style: MAPBOX_STYLEURI.LIGHT,
-            center: [12.9167, 50.8333],
-            zoom: 10,
-            renderWorldCopies: true,
-        });
-
-        this.map.on('style.load', () => {
-            // this.mapLoad();
-        });
-
-        this.map.on('load', () => {
-            // this.mapMouse();
-            if (this.showControls) {
-                this.addControls();
-            }
-        });
-    }
-
-    private addControls() {
-        this.map.addControl(new mapboxgl.NavigationControl(), 'top-right');
     }
 }
