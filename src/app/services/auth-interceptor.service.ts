063a6e9ce69289c971ea3c5e1d55a514b8238e18
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-06-07 18:30:45 +0200

    error status 0 handling

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -31,21 +31,20 @@ export class AuthInterceptorService implements HttpInterceptor {
         }
 
         req = req.clone({ headers: req.headers.set('Authorization', `Bearer ${accessToken}`) });
-        req = req.clone({ headers: req.headers.set('X-UID', uid) });
         req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
         req = req.clone({ headers: req.headers.set('Accept', 'application/json') });
+        req = req.clone({ headers: req.headers.set('X-UID', uid) });
 
         return next.handle(req)
             .pipe(
                 catchError((error: HttpErrorResponse) => {
-                    // Catching Error Stage
-                    if (error && error.status === 401) {
-                        console.log("ERROR 401 UNAUTHORIZED"); // in case of an error response the error message is displayed
+                    if (error.status === 0) {
+                        error.error.message = "Server is not running";
                     }
-                    const err = error.error.message || error.statusText;
-                    this.toastService.showErrorToast(err);
+                    const errorMessage = error.error.message || error.statusText;
+                    this.toastService.showErrorToast(errorMessage);
 
-                    if (err === "Invalid token") {
+                    if (errorMessage === "Invalid token") {
                         this.sessionService.clearSession();
                         this.router.navigateByUrl(AppSettings.RouteSignin);
                     }
