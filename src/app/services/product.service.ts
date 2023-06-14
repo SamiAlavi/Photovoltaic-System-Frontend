d8944a8b26f4317aadb70eb6c9080818746b29fb
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-06-14 19:40:13 +0200

    cache product types

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,14 +8,22 @@ import AppSettings from '../AppSettings';
 })
 export class ProductService {
     products: IProduct[] = [];
+    private products_key = 'products';
 
     constructor(private http: HttpClient) {
-        this.getProducts();
+        const products = sessionStorage.getItem(this.products_key);
+        if (products) {
+            this.products = JSON.parse(products);
+        }
+        else {
+            this.getProducts();
+        }
     }
 
     getProducts() {
         this.http.get<IProduct[]>(AppSettings.ProductUrl).subscribe((products) => {
             this.products = products;
+            sessionStorage.setItem(this.products_key, JSON.stringify(products));
         });
     }
 }
